63f31928df1b2d6d76b7406313358b9d0fc9f022
karlo-kasi <karlokasi1995@gmail.com>
2025-01-22 21:25:58 +0100

    Add: folder 3 - BONUS

--- snack-3_BONUS/main.js
+++ snack-3_BONUS/main.js
@@ -3,3 +3,24 @@ Scrivere una funzione che accetti tre argomenti, un array ( esempio: ['Michele',
 La funzione ritornerà un nuovo array con li elementi estratti dall'array di partenza che hanno la posizione compresa tra i due numeri (a e b).
 */
 
+
+//creo un ciclo
+let lista = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara']
+
+let newArray =[]
+
+function myFucntion (listaNuova, a , b ){
+
+    for (let i = 0; i < lista.length; i++){
+        
+        // se indice è maggiore uguale al parametro a e l'indice b è minore o uguale a l'indice 
+        if ( i >= (a - 1) && i <= ( b - 1)){
+           listaNuova.push(lista[i])
+           
+        } 
+
+    }
+    console.log(newArray)
+}
+
+myFucntion (newArray, 4 , 5)
