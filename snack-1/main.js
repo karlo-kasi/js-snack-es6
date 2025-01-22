777f7cbb6f1627cc5d7e7b3150683780881a4e1d
karlo-kasi <karlokasi1995@gmail.com>
2025-01-22 16:44:04 +0100

    Add: snack - 1 completed

--- snack-1/main.js
+++ snack-1/main.js
@@ -4,3 +4,28 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore.
 */
 
+let bici = [
+    { nome: "Bianchi", peso: 7.2 },
+    { nome: "Pinarello", peso: 8.1 },
+    { nome: "Specialized", peso: 6.8 },
+    { nome: "Trek", peso: 7.5 },
+    { nome: "Cannondale", peso: 7.1 },
+    { nome: "Scott", peso: 6.9 },
+    { nome: "Giant", peso: 8.0 }
+];
+  
+  
+  let pesoMinore = bici[0].peso
+
+
+  for (let i = 0; i < bici.length; i++){
+     
+
+     if (pesoMinore >= bici[i].peso){
+       leggera = bici[i]
+       
+     }
+
+}      
+console.log(leggera)
+    
